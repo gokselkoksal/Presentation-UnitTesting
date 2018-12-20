2124baba2473959f2ba6d746989cef57d463e77d
Göksel Köksal <gokselkoksal@gmail.com>
2018-12-21 00:57:53 +0300

    Initial Commit

---
+++ UnitTesting/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UnitTesting
+//
+//  Created by Göksel Köksal on 21.12.2018.
+//  Copyright © 2018 GK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
